f727262017583f1b778c33d9f38af49dd1e4dff7
Khushi Srivastava <khushi.srivastava145@gmail.com>
2021-07-13 13:06:02 +0530

    Added Accessing file manager to get file path

--- app/src/main/java/com/example/autogeneratecertificates/MainActivity.java
+++ app/src/main/java/com/example/autogeneratecertificates/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.autogeneratecertificates;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
-
+    Button browse;
+    TextView pathText;
+    String path_xlsx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        browse= (Button)findViewById(R.id.browseButton);
+        pathText= (TextView)findViewById(R.id.pathText);
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermission();
+            }
+        });
+
+
+    }
+    private void checkPermission() {
+        if(Build.VERSION.SDK_INT>=23){
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+            }
+            else{
+                getPath();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==1){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getPath();
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    protected void getPath() {
+        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+        i.setType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    path_xlsx = data.getData().getPath();
+                    Toast.makeText(this, path_xlsx, Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
