de24aea84f6186bce996a365d0e3354f2cc087f6
Meenakshi Bharadwaj <minakshi29bharadwaj@gmail.com>
2021-07-14 09:58:41 +0530

    Final changes with Splash screen addition

---
+++ app/src/main/java/com/example/autogeneratecertificates/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.autogeneratecertificates;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+
+        EasySplashScreen config = new EasySplashScreen( SplashScreenActivity.this )
+                .withFullScreen()
+                .withTargetActivity( MainActivity.class )
+                .withSplashTimeOut( 4000 )
+                .withBackgroundResource( R.drawable.splash_launcher_background )
+                .withHeaderText( "" )
+                .withFooterText( "Copyright 2021" )
+                .withBeforeLogoText( "" )
+                .withAfterLogoText( "CertiGen" )
+                .withLogo( R.mipmap.ic_launcher_round );
+
+        config.getHeaderTextView().setTextColor( Color.BLACK );
+        config.getAfterLogoTextView().setTextColor( Color.WHITE );
+        config.getBeforeLogoTextView().setTextColor( Color.BLUE );
+        config.getFooterTextView().setTextColor( Color.WHITE );
+
+        View splashscreen = config.create();
+        setContentView( splashscreen );
+    }
+}
